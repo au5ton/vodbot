223006d0ac49ca4ee8f7fb64f75728a9c456bcf9
Austin Jackson <austinjckson@gmail.com>
2018-06-21 03:27:17 -0500

    selenium example

---
+++ example.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get("https://www.twitch.tv/videos/275764362")
+#assert "Python" in driver.title
+#elem = driver.find_element_by_name("q")
+#elem.clear()
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
+sleep(10)
+#driver.close()
