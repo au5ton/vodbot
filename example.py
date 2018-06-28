740cf7e9d9cdaee9e460860a555540846be88c4d
Austin Jackson <austinjckson@gmail.com>
2018-06-28 01:45:26 -0500

    maintained authentication and go to different links

--- example.py
+++ example.py
@@ -1,8 +1,16 @@
+from time import sleep
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from time import sleep
 
-driver = webdriver.Chrome()
+# use already logged in user
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir=" + os.environ["CHROME_USER_DATA_DIR"])
+driver = webdriver.Chrome(executable_path=os.environ["CHROMEDRIVER_LOCATION"], chrome_options=options)
+
+# app logic
 driver.get("https://www.twitch.tv/videos/275764362")
 #assert "Python" in driver.title
 #elem = driver.find_element_by_name("q")
@@ -11,4 +19,6 @@ driver.get("https://www.twitch.tv/videos/275764362")
 #elem.send_keys(Keys.RETURN)
 #assert "No results found." not in driver.page_source
 sleep(10)
-#driver.close()
+driver.get("https://www.twitch.tv/videos/275764844")
+sleep(10)
+driver.close()
