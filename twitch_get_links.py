4876e4eb96645647ee69a48feca52e488181da9e
Austin Jackson <themacphage@gmail.com>
2019-06-29 00:43:26 -0500

    option to export vod data to CSV

--- twitch_get_links.py
+++ twitch_get_links.py
@@ -6,6 +6,8 @@ from twitch import TwitchClient
 import re
 import argparse
 from pprint import pprint
+import csv
+import json
 
 client = TwitchClient(os.environ["TWITCH_CLIENT_ID"])
 
@@ -19,30 +21,46 @@ parser.add_argument("-L", action="store", dest="limit", default="10", help="Limi
 parser.add_argument("-t", action="store", dest="broadcast_type", help="Broadcast type: `archive`, `highlight`, `upload`, `archive,upload`, `archive,highlight`, `highlight,upload`")
 parser.add_argument("-s", action="store", dest="sort", default="time", help="Sorting: `time`, `views`")
 parser.add_argument("-V", action="store_true", default=False, dest="get_videos", help="Get videos") # Only get what specified
-parser.add_argument("-A", action="store_true", default=False, dest="get_all_videos") # Specify this to loop through every vod available
+parser.add_argument("-links", action="store_true", default=False, dest="get_all_videos") # Specify this to loop through every vod available
+parser.add_argument("-csv", action="store_true", default=False, dest="make_csv") # Make a CSV of VOD data
 args = parser.parse_args()
 
 if(args.username is not None):
     users = client.users.translate_usernames_to_ids([args.username])
     for user in users:
+        args.userid = user.id
         print(user.id)
 
 if args.userid is not None:
     all_videos = []
     total_got = 0
     last_req_length = 1
+    print("Getting ALL videos...")
+    while last_req_length > 0:
+        #print("\nGetting 100 more (if possible)...")
+        videos = client.channels.get_videos(channel_id=args.userid, limit=100, offset=total_got, broadcast_type=args.broadcast_type, sort=args.sort)
+        total_got += len(videos)
+        last_req_length = len(videos)
+        all_videos += videos
+        print("\tTotal fetched: "+str(len(all_videos)))
     if args.get_all_videos:
-        print("Getting ALL videos...")
-        while last_req_length > 0:
-            #print("\nGetting 100 more (if possible)...")
-            videos = client.channels.get_videos(channel_id=args.userid, limit=100, offset=total_got, broadcast_type=args.broadcast_type, sort=args.sort)
-            total_got += len(videos)
-            last_req_length = len(videos)
-            all_videos += videos
-            print("\tTotal fetched: "+str(len(all_videos)))
         with open(args.output_file_name, "w") as text_file:
             for video in all_videos:
                 text_file.write(video.url + "\n")
+    elif args.make_csv:
+        with open(args.output_file_name, "w") as csvfile:
+            spam = csv.writer(csvfile, dialect='excel')
+            # write headers
+            spam.writerow(all_videos[0])
+            # write data
+            for vod in all_videos:
+                row = []
+                for attr in vod:
+                    if type(vod[attr]) is not object:
+                        row += [vod[attr]]
+                    else:
+                        row += json.dumps(vod[attr])
+                spam.writerow(row)
     else:
         videos = client.channels.get_videos(channel_id=args.userid, limit=int(args.limit), offset=args.offset, broadcast_type=args.broadcast_type, sort=args.sort)
         with open(args.output_file_name, "w") as text_file:
